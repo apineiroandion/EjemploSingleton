41501a89d542b1b49b38051ef2f6ffca07724946
angel <apineiroandion@danielcastelao.org>
2024-04-12 09:59:28 +0200

    añado DisplayName al test

--- test/TestDato.java
+++ test/TestDato.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import  org.junit.jupiter.api.Assertions;
@@ -5,6 +6,7 @@ import  org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.*;
 public class TestDato {
     @Test
+    @DisplayName("Comprobar misma instancia")
     public void assertSameDatoReturnTrue(){
         Dato dato1 = Dato.getInstance();
         Dato dato2 = Dato.getInstance();
